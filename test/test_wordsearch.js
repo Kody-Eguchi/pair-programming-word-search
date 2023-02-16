16a349238162f60d7e69f8195e56a655d4447fb4
Koji Eguchi <koji.eguchi@hotmail.com>
2023-02-15 19:48:37 -0500

    completed assignment

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -1,7 +1,7 @@
 const chai = require('chai');
 const assert = chai.assert;
 
-const wordSearch = require('../wordsearch.js')
+const wordSearch = require('../wordsearch.js');
 
 describe("#wordSearch()", function() {
   it("should return false if the word is not present", function() {
@@ -15,12 +15,11 @@ describe("#wordSearch()", function() {
       ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
       ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
       ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
-    ], 'FRANK')
-
+    ], 'FRANK');
     assert.isFalse(result);
   });
 
-  it("should return true if the word is present", function() {
+  it("should return true if the word is present in a horizontal line", function() {
     const result = wordSearch([
       ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
       ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
@@ -31,8 +30,41 @@ describe("#wordSearch()", function() {
       ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
       ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
       ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
-    ], 'SEINFELD')
+    ], 'SEINFELD');
+    assert.isTrue(result);
+  });
 
+  it("should return true if the array is empty", function() {
+    const result = wordSearch([], 'apple');
+    assert.isUndefined(result);
+  });
+  it("should return true if the word is not passed", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'P', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'P', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'V', 'L', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'E', 'E', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'P', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'I', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'E', 'G'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'S', 'L'],
+    ]);
+    assert.isUndefined(result);
+  });
+
+  it("should return true if the word is present in a vertical line", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'P', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'P', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'V', 'L', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'E', 'E', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'P', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'I', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'E', 'G'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'S', 'L'],
+    ], 'APPLEPIES');
     assert.isTrue(result);
   });
 });
--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,50 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+const wordSearch = (letters, word) => {
+//----------------Empty Array---------------//
+  if (letters.length === 0) {
+    console.log('Please pass a matrix');
+    return undefined;
+  }
+  //----------------Empty Words---------------//
+  if (word === undefined) {
+    console.log('Please pass a word');
+    return undefined;
+  }
 
-module.exports = wordSearch
+  //----------------Vertical---------------//
+  let verticalJoin = [];
+  for (let i = 0; i < letters[0].length; i++) {
+    verticalJoin.push([]);
+  }
+
+  for (let x = 0; x < letters.length; x++) {
+    for (let y = 0; y < letters[x].length; y ++) {
+      verticalJoin[y].push(letters[x][y]);
+    }
+  }
+
+
+  const verticalJoin2 = verticalJoin.map(ls => ls.join(''));
+  for (const vl of verticalJoin2) {
+    if (vl.includes(word)) return true;
+  }
+
+
+  //----------------horizontal---------------//
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  for (const hl of horizontalJoin) {
+    if (hl.includes(word)) return true;
+  }
+
+  return false;
+};
+
+
+module.exports = wordSearch;
+
+// ✅ what if word is written vertically but not horizontally?
+// ✅ what if matrix is an empty array?
+// ✅ empty word
+// ✅ add any cases we think are necessary
+// ✅write tests for these cases
+
+
